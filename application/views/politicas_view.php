f79712ac09b665ac84424c5dbc9df169237ea451
JL__CRUZ <cruz@cerbero.com.mx>
2017-06-13 11:26:22 -0600

    politicas de ventas actualización

--- application/views/politicas_view.php
+++ application/views/politicas_view.php
@@ -3,7 +3,7 @@
     <div class="section-shade sep-top-5x sep-bottom-3x">
       <div class="container">
         <div class="section-title upper light">
-          <h2 class="small-space">Políticas</h2>
+          <h2 class="small-space">Políticas de ventas</h2>
         </div>
       </div>
     </div>
@@ -18,34 +18,39 @@
                     <label>PRECIOS</label> <br><br>
                     A) Sujetos a cambio sin previo aviso.<br>
                     B) No incluyen flete, maniobras, ni empaques especiales requeridos.<br>
-                    C) Para reservar un artículo es necesario depositar 50% del valor, y solamente se reservará durante 15 días hábiles, transcurrido este tiempo, el artículo se liberará y quedará  como saldo a favor el 50% depositado. La vigencia de las cotizaciones es de cinco días naturales.<br>
-                    D) Al realizar un depósito es indispensable enviar la ficha bancaria por correo electrónico a la persona de ventas que lo atendió, incluyendo su número de cotización y su código de cliente, en caso de contar con él. <br>
+                    C) Para reservar un artículo es necesario depositar 50% del valor, y solamente se reservará durante 15 días hábiles, transcurrido este tiempo, el artículo se liberará y quedará como saldo a favor el 50% depositado. La vigencia de las cotizaciones es de cinco días naturales.<br>
+                    D) Al realizar un depósito es indispensable enviar la ficha bancaria por correo electrónico a la persona de ventas que lo atendió, incluyendo su número de cotización y su código de cliente, en caso de contar con el.<br>
                     <br>
                     <label>FACTURACION</label> <br>
-                    A) Es responsabilidad del cliente solicitar la factura al momento de efectuar la compra, de lo contrario no se podrá facturar. El cliente deberá asegurarse que sus datos de facturación sean correctos, ya que una vez efectuada la factura, no podrá ser emitida nuevamente. 
+                    A) Es responsabilidad del cliente solicitar la factura al momento de efectuar la compra, de lo contrario no se podrá facturar. El cliente deberá asegurarse que sus datos de facturación sean correctos, ya que una vez efectuada la factura, no podrá ser emitida nuevamente.
                     <br><br>
                     <label>PRODUCTOS</label><br>
-                    A) Nuestro mobiliario y artículos decorativos no incluyen accesorios, tales como vidrios, velas, focos, cojines, etc. excepto cuando éstos se especifiquen en la etiqueta.<br>
+                    A) Nuestro mobiliario y artículos decorativos no incluyen accesorios, tales como vidrios, velas, focos, cojines, etc. excepto cuando éstos se especifiquen en la etiqueta. <br>
                     B) Nuestros productos son únicos o fabricados a mano, por lo que entre ellos siempre se apreciará alguna pequeña variante de color o textura.<br>
                     C) Nuestro tiempo de producción será de 6 a 8 semanas a partir de la entrega del anticipo.<br>
-                    D) Para los productos de  producción se requiere del 70% de anticipo para que se mande a producir. 
+                    D) Para los productos de producción se requiere del 70% de anticipo para que se mande a producir.
                     <br><br>
                     <label>ALMACENAJE</label> <br>
-                    A) Los pedidos que no sean recolectados después de un mes, estarán sujetos a un costo de almacenaje del 5% mensual de su valor total. 
+                    A) Los pedidos que no sean recolectados después de 15 días, estarán sujetos a un costo de almacenaje del 5% mensual de su valor total.
                     <br><br>
                     <label>DEVOLUCIONES</label> <br>
-                    A) Los pedidos confirmados y pagados total o parcialmente serán reembolsables únicamente en mercancía o nota de crédito. <br>
-                    B) No habrá devoluciones transcurrido un mes a partir de la compra. No se aceptarán devoluciones de mercancía en liquidación o piezas específicas como: cojines, velas, telas, tapetes, cerámica y vidrio. No existe devolución en artículos de producción especial, ventas semi anuales, o en artículos previamente señalados al momento de la compra. 
+                    A) Los pedidos confirmados y pagados total o parcialmente serán reembolsables únicamente en mercancía o nota de crédito.  <br>
+                    B) No habrá devoluciones transcurrido un mes a partir de la compra. No se aceptarán devoluciones de mercancía en liquidación o piezas específicas como: cojines, velas, telas, tapetes, cerámica y vidrio. No existe devolución en artículos de producción especial, ventas semi anuales, o en artículos previamente señalados al momento de la compra. Ni en obras de arte de la Galería Namuh
                     <br><br>
                     <label>VISTAS</label> <br> 
-                    A) Excepcionalmente se podrá autorizar el traslado de mercancía a vistas, solamente con garantía de pago en tarjeta de crédito o efectivo por el valor total.<br>
+                    A) Excepcionalmente se podrá autorizar el traslado de mercancía a vistas, solamente con garantía de pago en tarjeta de crédito o efectivo por el valor total. <br>
                     B) Se concederán 5 días hábiles dentro de los estados de Querétaro y Guanajuato y 15 días en el resto de la República para la confirmación. <br>
-                    C) Si la mercancía presenta daño, el cliente tendrá que cubrir  el costo de reparación o pago total si ese fuere el caso.<br>
-                    D) Si transcurrido este período la mercancía no es regresada a tiempo  la garantía no se devolverá aun cuando la mercancía sea retornada y quedará como saldo a favor para futuras compras. El cliente cubrirá los costos del flete de esta mercancía. 
+                    C) Si la mercancía presenta daño, el cliente tendrá que cubrir el costo de reparación o pago total si ese fuere el caso. <br>
+                    D) Si transcurrido este período la mercancía no es regresada a tiempo la garantía no se devolverá aun cuando la mercancía sea retornada y quedará como saldo a favor para futuras compras. El cliente cubrirá los costos del flete de esta mercancía.
+                    <br><br>
+                    <label>REEMBOLSOS</label><br>
+                    A) Son con excepciones y se analizará el caso.<br>
+                    B ) En caso de solicitar un reembolso y el pago se haya realizado con tarjeta de crédito o de débito NAMUH no absorberá las comisiones y se descontarán del monto del reembolso
                     <br><br>
                     <label>ENVIOS</label> <br>
-                    <label>POLÍTICAS DE VENTAS</label> <br>
-                    A) En caso de que el cliente solicite el envío de su mercancía a su domicilio, Namuh procederá a cotizar con varios transportistas independientes, para informar al cliente de las opciones y ayudar a coordinar el envío. Quedando por entendido que Namuh no es responsable por daños o robos en el envío, y el cliente deberá contratar un envío con seguro que ampare la mercancía.<br> En caso contrario, la responsabilidad total será del cliente.</p>
+                    A) En caso de que el cliente solicite el envío de su mercancía a su domicilio, Namuh procederá a cotizar con varios transportistas independientes, para informar al cliente de las opciones y ayudar a coordinar el envío. Quedando por entendido que Namuh no es responsable por daños o robos en el envío, y el cliente deberá contratar un envío con seguro que ampare la mercancía. En caso contrario, la responsabilidad total será del cliente.
+
+                    </p>
               </div>
             </div>
           </div>
@@ -62,4 +67,3 @@
           </div>
         </div>
       </div>
-      
